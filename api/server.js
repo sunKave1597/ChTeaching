1b61ea48890b8bfc7b32136ece65ee413a78f22b
Sunny .dev <teeseeker1015@gmail.com>
2025-09-27 18:23:30 +0700

    test

--- api/server.js
+++ api/server.js
@@ -11,6 +11,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const PORT = process.env.PORT || 5000; 
+app.listen(PORT, () => {
+    console.log(`✅ Server running successfully on http://localhost:${PORT}`);
+});
+
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
