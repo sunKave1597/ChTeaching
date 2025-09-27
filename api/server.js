aebecdef853433b6da87123ead3155caec6cc1bb
Sunny .dev <teeseeker1015@gmail.com>
2025-09-27 15:33:30 +0700

    feat: enhance server initialization with MongoDB connection and updated response message

--- api/server.js
+++ api/server.js
@@ -1,18 +1,39 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const cors = require("cors");
+const path = require("path");
+dotenv.config();
+const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        });
+
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1); 
+    }
+};
+
+connectDB();
+
+
 app.get("/", (req, res) => {
-  res.send("API is running 🚀");
+    res.send("API is running and MongoDB is connected 🚀");
 });
 
-const userRoutes = require("./routes/userRoutes");
+const userRoutes = require("./routes/userRoutes"); 
 app.use("/api/users", userRoutes);
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
+    console.log(`✅ Server running on port ${PORT}`);
 });
