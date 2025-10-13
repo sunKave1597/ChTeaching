039d4b1cd9ed168367ae57cf65221136ce44a654
Sunny .dev <teeseeker1015@gmail.com>
2025-10-13 17:08:24 +0700

    refactor: switch to serverless architecture by exporting app with serverless-http

--- api/server.js
+++ api/server.js
@@ -19,5 +19,6 @@ app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/words", wordRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+const serverless = require("serverless-http");
+module.exports = serverless(app);
+
