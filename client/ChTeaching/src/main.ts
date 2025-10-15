0fde13f7d840d651002f3c24d4e66783d700085d
Sunny .dev <teeseeker1015@gmail.com>
2025-10-15 19:08:12 +0700

    fix: update bootstrap paths and rename AppComponent to App in main.ts

--- client/ChTeaching/src/main.ts
+++ client/ChTeaching/src/main.ts
@@ -1,11 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-import { AppComponent } from './app/app';
+import { App } from './app/app';
 import { routes } from './app/app.routes';
 import { AuthService } from './auth.service';
 
-bootstrapApplication(AppComponent, {
+bootstrapApplication(App, {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
