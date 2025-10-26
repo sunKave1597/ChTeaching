26d8a1cad4cb7f11d0ffc60c88303cd3fb8065ca
Sunny .dev <teeseeker1015@gmail.com>
2025-10-26 20:17:59 +0700

    fix: remove request body size limits for JSON and URL-encoded data in server configuration

--- api/server.js
+++ api/server.js
@@ -15,8 +15,7 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(express.json({ limit: "20mb" }));
-app.use(express.urlencoded({ extended: true, limit: "20mb" }));
+
 app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => res.send("API is running 🚀"));
